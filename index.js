6af5e3037eb87650bf3eefc7bbf0ffd196301c18
lisiurday <lisiurday@gmail.com>
2017-10-23 08:05:26 +0800

    update api

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const axios = require('axios')
 const md5 = require('md5')
 
-const __emit__ = function({ url, username, appname, createdAt, uuid, request, response, labels = [] }) {
-  return axios.post(url, {
+const __emit__ = function({ path, username, appname, createdAt, uuid, request, response, labels = [] }) {
+  return axios.post(path, {
     username, appname, createdAt, uuid, request, response, labels
   })
 }
 
 // 自动创建 createdAt
-const _emit = function({url, username, appname, uuid, labels = []}) {
+const _emit = function({path, username, appname, uuid, labels = []}) {
   return function({request, response}) {
     const createdAt = new Date()
-    return __emit__({ url, username, appname, createdAt, uuid, request, response, labels })
+    return __emit__({ path, username, appname, createdAt, uuid, request, response, labels })
   }
 }
 
-// 传递 url username appname labels
+// 传递 path username appname labels
 // 自动创建 uuid
-const watcher = function({watcherServerUrl, username, appname, labels = []}) {
+const watcher = function({path, origin, username, appname, labels = []}) {
   const uuid = md5(new Date().toString() + Math.random())
+  path = path || origin + '/receiver'
   return {
     emitReq: function(request) {
       let {url, headers, params, method, queries} = request
       request = {url, headers, params, method, queries}
-      return _emit({url: watcherServerUrl, username, appname, uuid, labels})({request})
+      return _emit({path, origin, username, appname, uuid, labels})({request})
     },
     emitRes: function(response) {
       let {status, data, headers} = response
       response = {status, data, headers}
-      return _emit({url: watcherServerUrl, username, appname, uuid, labels})({response})
+      return _emit({path, username, appname, uuid, labels})({response})
     }
   }
 }
